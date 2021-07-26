d6e2f85e42137b653fce87eff366bec802644c22
kburd <kalebmburd@gmail.com>
2021-07-26 10:52:48 -0400

    kdawg chabges

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -2,7 +2,19 @@ package hbcu.stay.ready.algorithms;
 
 public class WuTangForeverBuzz {
 
-    public String wuTangClan(){
-        return null;
+    public String wuTangClan(int n){
+
+        StringBuilder output = new StringBuilder("1");
+
+        for(int i = 2; i <= n; i++){
+            output.append("\n");
+            if(i%3==0) output.append("Wu");
+            if(i%5==0) output.append("Tang");
+            if(i%15==0) output.append(" Forever");
+            if(i%3!=0 && i%5!=0) output.append(i);
+        }
+
+        return output.toString();
+
     }
 }
--- src/test/java/hbcu/stay/ready/algorithms/WuTangForeverBuzzTest.java
+++ src/test/java/hbcu/stay/ready/algorithms/WuTangForeverBuzzTest.java
@@ -6,12 +6,23 @@ import org.junit.Test;
 public class WuTangForeverBuzzTest {
 
     @Test
-    public void wuTangClanTest(){
+    public void wuTangClanTestOne(){
         WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
 
         String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTang Forever";
-        String actual = wuTangForeverBuzz.wuTangClan();
+        String actual = wuTangForeverBuzz.wuTangClan(15);
 
         Assert.assertEquals(expected,actual);
     }
+
+    @Test
+    public void wuTangClanTestTwo(){
+        WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
+
+        String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTang Forever\n16\n17\nWu\n19\nTang\nWu\n22\n23\nWu\nTang\n26\nWu\n28\n29\nWuTang Forever\n31\n32\nWu\n34\nTang\nWu\n37\n38\nWu\nTang";
+        String actual = wuTangForeverBuzz.wuTangClan(40);
+
+        Assert.assertEquals(expected,actual);
+    }
+
 }
