ecb12b9184ef473d0e7e979ab9dddae50fb63bd7
kburd <kalebmburd@gmail.com>
2021-07-26 10:57:28 -0400

    removing solution and tweaking test

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,18 +3,6 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(int n){
-
-        StringBuilder output = new StringBuilder("1");
-
-        for(int i = 2; i <= n; i++){
-            output.append("\n");
-            if(i%3==0) output.append("Wu");
-            if(i%5==0) output.append("Tang");
-            if(i%15==0) output.append(" Forever");
-            if(i%3!=0 && i%5!=0) output.append(i);
-        }
-
-        return output.toString();
-
+        return null;
     }
 }
